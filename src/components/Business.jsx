e11f0cb0c0f8dcce09f3b7ce9c001325349eb04c
svartse <killbillbeatrixx@gmail.com>
2023-05-19 20:16:31 +0300

    css changed

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from './Button';
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden"/> we’ll handle the money.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <Button styles="mt-10" />
+      </div>
+    </section>
   )
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>Button</div>
+    <button type='button' className={`py-4 px-6 bg-purple-gradient rounded-lg font-poppins font-medium text-[18px] text-white outline-none ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
