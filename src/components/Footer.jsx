746211edabdbac924c849b1e4a510bfd5a6a7a52
svartse <killbillbeatrixx@gmail.com>
2023-07-01 17:45:09 +0300

    footer component complated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,14 +15,29 @@ const Footer = () => (
               <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
                 {footerLink.title}
               </h4>
-              <ul>
+              <ul className='list-none mt-4'>
                 {footerLink.links.map((link, index) => (
-                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                     {link.name}
                   </li>
                 ))}
               </ul>
             </div>
+          ))}
+        </div>
+      </div>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
+        <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+          2023 GoBank. All Rights Reserved. 
+        </p>
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((social, index) => (
+            <img 
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mb-6' : 'mb-0'}`}
+            />
           ))}
         </div>
       </div>
