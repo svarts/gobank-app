9cb5f063835feebb20bb2c1a535675eb85438dc4
svartse <killbillbeatrixx@gmail.com>
2023-07-01 13:41:33 +0300

    footer classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import styles from '../style';
 import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => (
-    <section className=''>
-      <div>
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
 
       </div>
     </section>
