c54d092343a64c52b55b7a21a3d1199001d5e954
svartse <killbillbeatrixx@gmail.com>
2023-07-01 13:47:37 +0300

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { footerLinks, socialMedia } from '../constants';
 const Footer = () => (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-
+        <div><img src="https://ik.imagekit.io/laracasts/series/thumbnails/svg/developing-serverless-functions.svg" alt="" /></div>
       </div>
     </section>
 )
