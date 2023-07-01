585e10b09932d37ac4524c0d96cc5e0de5972acc
svartse <killbillbeatrixx@gmail.com>
2023-07-01 17:56:13 +0300

    footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
         <div className='flex-1 flex flex-col justify-start mr-10'>
           <img src="https://ik.imagekit.io/laracasts/series/thumbnails/svg/developing-serverless-functions.svg" alt="gobank" 
-          className='w-[300px] h-[300px] object-contain'/>
+          className='w-[280px] h-[280px] object-contain'/>
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
